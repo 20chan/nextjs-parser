800f394fa080015a97b1c3171cdf232f87b4cacc
20chan <2@0chan.dev>
2023-12-04 18:18:21 +0900

    init commit

---
+++ Readme.md
@@ -0,0 +1,122 @@
+# nextjs-parser
+
+NextJS page props, especially UrlSearchParams parser utility in NextJS v13 app routing.
+
+In NextJS v13 app routing, [Page](https://nextjs.org/docs/app/api-reference/file-conventions/page) can get params, searchParams from url.
+But searchParams is not URLSearchParams, it is just plain object. So this library parse it to schema and make url navigation easy.
+
+There will be support for react's FormData type used in server action in a similar way.
+
+## Installation
+
+```bash
+npm install nextjs-parser
+```
+
+## Example
+
+Pagination like `/posts?page=1` can be parsed like this.
+
+`paginationParams.ts`
+
+```typescript
+import { NextProps, NextSearchParams } from '../NextProps';
+
+export interface PaginationParams {
+  page: number;
+}
+
+export namespace PaginationParams {
+  const schema = {
+    page: 'number',
+  } as const;
+
+  export const defaultValue: PaginationParams = {
+    page: 1,
+  };
+
+  export const parse = (props: NextProps): PaginationParams => {
+    return NextSearchParams.parse(props.searchParams, schema, defaultValue);
+  };
+
+  export const merge = (searchParams: NextSearchParams, params: Partial<PaginationParams>): string => {
+    return NextSearchParams.merge(searchParams, params);
+  }
+}
+```
+
+`page.tsx`
+
+```typescript
+
+export default async function ListPage(props: NextProps) {
+  const { page } = PaginationParams.parse(props);
+
+  return (
+    <Link href={`/page?${
+      PaginationParams.merge(props.searchParams, {
+        page: page + 1,
+      })
+    }`}>
+      next page
+    </Link>
+  )
+  // ...
+}
+```
+
+### Nullable
+
+`paginationParams.ts`
+
+```typescript
+import { NextProps, NextSearchParams } from '../NextProps';
+
+export interface CreatePaginationParams {
+  create: boolean | null;
+  page: number;
+}
+
+export namespace CreatePaginationParams {
+  const schema = {
+    create: 'boolean?',
+    page: 'number',
+  } as const;
+
+  export const defaultValue: CreatePaginationParams = {
+    create: null,
+    page: 1,
+  };
+
+  export const parse = (props: NextProps): CreatePaginationParams => {
+    return NextSearchParams.parse(props.searchParams, schema, defaultValue);
+  };
+
+  export const merge = (searchParams: NextSearchParams, params: Partial<CreatePaginationParams>): string => {
+    return NextSearchParams.merge(searchParams, params);
+  }
+}
+```
+
+`page.tsx`
+
+```typescript
+export default async function ListPage(props: NextProps) {
+  const { create, page } = CreatePaginationParams.parse(props);
+
+  if (create) {
+    // ...
+  }
+
+  return (
+    <Link href={`/page?${
+      CreatePaginationParams.merge(props.searchParams, {
+        page: page + 1,
+      })
+    }`}>
+      next page
+    </Link>
+  )
+  // ...
+}
+```
---
+++ src/NextProps.ts
@@ -0,0 +1,231 @@
+export type NextParams = { slug: string };
+export type NextSearchParams = Record<string, string | string[]>;
+
+export type NextProps = {
+  params: NextParams
+  searchParams: NextSearchParams
+};
+
+export namespace NextSearchParams {
+  export const get = (searchProps: NextSearchParams, key: string): string[] | null => {
+    const keyEscaped = key.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+    const value = searchProps[keyEscaped];
+
+    if (value === undefined) {
+      return null;
+    }
+    if (typeof value === 'string') return [value];
+    if (Array.isArray(value)) return value;
+    return null;
+  };
+
+  export const getStrings = get;
+
+  export const getString = (searchProps: NextSearchParams, key: string): string | null => {
+    const value = get(searchProps, key);
+
+    if (value === null) {
+      return null;
+    }
+
+    return value[0];
+  };
+
+  export const getNumbers = (searchProps: NextSearchParams, key: string): number[] => {
+    const valueRaw = get(searchProps, key);
+
+    if (valueRaw === null) {
+      return [];
+    }
+
+    const value = valueRaw.map(value => parseInt(value));
+    if (value.some(value => isNaN(value))) {
+      return [];
+    }
+
+    return value;
+  }
+
+  export const getNumber = (searchProps: NextSearchParams, key: string): number | null => {
+    const value = getNumbers(searchProps, key);
+
+    if (value.length === 0) {
+      return null;
+    }
+
+    return value[0];
+  }
+
+  export const getBooleans = (searchProps: NextSearchParams, key: string): boolean[] => {
+    const valueRaw = get(searchProps, key);
+
+    if (valueRaw === null) {
+      return [];
+    }
+
+    const value = valueRaw.map(value => value === 'true' || value === '');
+    if (value.some(value => typeof value !== 'boolean')) {
+      return [];
+    }
+
+    return value;
+  }
+
+  export const getBoolean = (searchProps: NextSearchParams, key: string): boolean | null => {
+    const value = getBooleans(searchProps, key);
+
+    if (value.length === 0) {
+      return null;
+    }
+
+    return value[0];
+  }
+
+  type SchemaValue = (
+    | 'string'
+    | 'string?'
+    | 'string[]'
+    | 'number'
+    | 'number?'
+    | 'number[]'
+    | 'boolean'
+    | 'boolean?'
+    | 'boolean[]'
+  );
+
+  type ScheamValueType = (
+    | string
+    | string[]
+    | number
+    | number[]
+    | boolean
+    | boolean[]
+    | null
+  );
+
+  type ParseSchemaValueType<T extends SchemaValue> = (
+    T extends 'string' ? string :
+    T extends 'string?' ? string | null :
+    T extends 'string[]' ? string[] :
+    T extends 'number' ? number :
+    T extends 'number?' ? number | null :
+    T extends 'number[]' ? number[] :
+    T extends 'boolean' ? boolean :
+    T extends 'boolean?' ? boolean | null :
+    T extends 'boolean[]' ? boolean[] :
+    never
+  );
+
+  type SchemaFieldDefinition = SchemaValue;
+
+  type TSchemaBase = {
+    [key: string]: SchemaFieldDefinition
+  }
+
+  type RawParseResult<TSchema extends TSchemaBase> = {
+    [key in keyof TSchema]: ParseSchemaValueType<TSchema[key]>;
+  }
+
+  type SchemaDefaultValue<TSchema extends TSchemaBase> = Partial<RawParseResult<TSchema>>;
+
+  type ExtractSchemaDefaultValueType<
+    TSchema extends TSchemaBase,
+    TDefaultValue extends SchemaDefaultValue<TSchema>,
+    key extends keyof TSchema,
+  > = TDefaultValue[key] extends undefined ? null : TDefaultValue[key];
+
+  type ParseResult<TSchema extends TSchemaBase, TDefaultValue extends SchemaDefaultValue<TSchema>> = {
+    [key in keyof TSchema]: ExtractSchemaDefaultValueType<TSchema, TDefaultValue, key>;
+  };
+
+  export const parse = <TSchema extends TSchemaBase, TDefaultValue extends SchemaDefaultValue<TSchema>>(
+    searchProps: NextSearchParams,
+    schema: TSchema,
+    defaultValue?: TDefaultValue,
+  ): ParseResult<TSchema, TDefaultValue> => {
+    const result = {} as ParseResult<TSchema, TDefaultValue>;
+
+    for (const key in schema) {
+      const type = schema[key];
+      const fallback = defaultValue?.[key] === undefined ? null : defaultValue[key];
+
+      if (type === 'string[]') {
+        result[key] = (getStrings(searchProps, key) ?? fallback) as any;
+      } else if (type === 'string' || type === 'string?') {
+        result[key] = (getString(searchProps, key) ?? fallback) as any;
+      } else if (type === 'number[]') {
+        result[key] = (getNumbers(searchProps, key) ?? fallback) as any;
+      } else if (type === 'number' || type === 'number?') {
+        result[key] = (getNumber(searchProps, key) ?? fallback) as any;
+      } else if (type === 'boolean[]') {
+        result[key] = (getBooleans(searchProps, key) ?? fallback) as any;
+      } else if (type === 'boolean' || type === 'boolean?') {
+        result[key] = (getBoolean(searchProps, key) ?? fallback) as any;
+      } else {
+        throw new Error(`Invalid schema value: ${type}`);
+      }
+    }
+
+    return result;
+  };
+
+  export const qsfy = (searchParams: NextSearchParams): URLSearchParams => {
+    const qs = new URLSearchParams();
+
+    for (const key in searchParams) {
+      const value = searchParams[key];
+      if (value === null) {
+        continue;
+      }
+      if (typeof value === 'string') {
+        qs.append(key, value);
+      } else if (Array.isArray(value)) {
+        for (const valueElement of value) {
+          qs.append(key, valueElement);
+        }
+      }
+    }
+
+    return qs;
+  };
+
+  export const merge = <TValue extends Record<string, ScheamValueType>>(
+    searchParams: NextSearchParams,
+    params: TValue,
+  ): string => {
+    const qs = qsfy(searchParams);
+
+    for (const key in params) {
+      if (qs.has(key)) {
+        qs.delete(key);
+      }
+      const value = params[key];
+      if (value === null) {
+        continue;
+      }
+      if (typeof value === 'string') {
+        qs.append(key, value);
+      } else if (typeof value === 'number') {
+        qs.append(key, value.toString());
+      } else if (typeof value === 'boolean') {
+        qs.append(key, value.toString());
+      } else if (Array.isArray(value)) {
+        for (const valueElement of value) {
+          if (typeof valueElement === 'string') {
+            qs.append(key, valueElement);
+          } else if (typeof valueElement === 'number') {
+            qs.append(key, valueElement.toString());
+          } else if (typeof valueElement === 'boolean') {
+            qs.append(key, valueElement.toString());
+          } else {
+            throw new Error(`Invalid value element: ${valueElement}`);
+          }
+        }
+      } else {
+        throw new Error(`Invalid value: ${value}`);
+      }
+    }
+
+    return qs.toString();
+  };
+}
